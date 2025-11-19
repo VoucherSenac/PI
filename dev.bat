ace7fa2e8c3861ea177aa1c58553999940229150
Arthur <senacvoucher@gmail.com>
2025-11-18 21:06:33 -0400

    ads

---
+++ dev.bat
@@ -0,0 +1,22 @@
+@echo off
+echo ========================================
+echo   Sistema de Gerenciamento Hospitalar
+echo ========================================
+echo.
+
+echo Abrindo navegador padrao no localhost:8000...
+start cmd /c explorer "http://localhost:8000"
+
+echo Navegando para o diretorio do projeto...
+cd c:/Users/tlglab203/Documents/PI
+
+echo Executando migrations...
+php artisan migrate
+
+echo Executando seeders...
+php artisan db:seed
+
+echo Executando composer run dev...
+composer run dev
+
+pause
